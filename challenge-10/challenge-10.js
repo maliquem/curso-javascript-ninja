0befaf616624a90775b39371ae7b09040051a102
Jael Araripe Rabelo <47405323+maliquem@users.noreply.github.com>
2022-05-12 11:40:40 -0300

    Update challenge-10.js

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -53,12 +53,7 @@ Caso contrário, "false".
 - O desafio é fazer o retorno sem usar "if" ou "switch".
 */
 function isOperatorValid(operador){
-    var valid = operador === '+' ? true :
-    operador === '-' ? true :
-    operador === '*' ? true :
-    operador === '/' ? true :
-    operador === '%' ? true : false;
-    return valid;
+    return !!operation[operador];
 }
 
 /*
@@ -80,7 +75,7 @@ function calculator(operador){
     }
 
     return function(x, y) {
-        if (typeof x !== 'number' && typeof y !== 'number') {
+        if (typeof x !== 'number' || typeof y !== 'number') {
             return false;
         }
 
@@ -147,7 +142,29 @@ Repita desde o "PASSO 2" com as operações de subtração, multiplicação,
 divisão e resto. Crie variáveis com os nomes "subtraction",
 "multiplication", "division" e "mod".
 */
-// ?
+operationSignal = '-';
+var subtraction = calculator(operationSignal);
+subtraction !== false ? 
+console.log(showOperationMessage(operationSignal, number1, number2), subtraction(number1, number2)) : 
+console.log(showErrorMessage(operationSignal));
+
+operationSignal = '*';
+var multiplication = calculator(operationSignal);
+multiplication !== false ? 
+console.log(showOperationMessage(operationSignal, number1, number2), multiplication(number1, number2)) : 
+console.log(showErrorMessage(operationSignal));
+
+operationSignal = '/';
+var division = calculator(operationSignal);
+division !== false ? 
+console.log(showOperationMessage(operationSignal, number1, number2), division(number1, number2)) : 
+console.log(showErrorMessage(operationSignal));
+
+operationSignal = '%';
+var mod = calculator(operationSignal);
+mod !== false ? 
+console.log(showOperationMessage(operationSignal, number1, number2), mod(number1, number2)) : 
+console.log(showErrorMessage(operationSignal));
 
 /*
 Repita o PASSO 2 novamente, mas passando um operador inválido, para ver se
